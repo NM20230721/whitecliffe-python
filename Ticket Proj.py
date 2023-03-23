73c58b3639915e331cf2711020cfc3a6be1f4aa4
NM20230721 <126734989+NM20230721@users.noreply.github.com>
2023-03-24 12:15:53 +1300

    Add files via upload

--- Ticket Proj.py
+++ Ticket Proj.py
@@ -1,9 +1,10 @@
 class Ticket():
-
+    #Counting Tickets
     total_tickets = 0
     open_tickets = 0
     closed_tickets = 0
 
+    #methods for retrieving ticket analytics
     @classmethod
     def getTickets(cls):
         return Ticket.open_tickets
@@ -15,16 +16,30 @@ class Ticket():
     def getClosedTickets(cls):
         return Ticket.closed_tickets
 
+    @classmethod
+    def displayTicketStats(cls):
+        print("-------STATS--------")
+        print("Total Tickets: ",Ticket.total_tickets)
+        print("Open Tickets: ", Ticket.open_tickets)
+        print("Closed Tickets: ", Ticket.closed_tickets)
+        print("--------------------")
+
+    #intialising the ticket object
     def __init__(self, staff_id, ticket_creator_name, contact_email, desc):
         Ticket.total_tickets += 1
         Ticket.open_tickets += 1
+        self.ticket_number = Ticket.total_tickets + 2000
         self.staff_id = staff_id
         self.ticket_creator_name = ticket_creator_name
         self.contact_email = contact_email
         self.desc = desc
         self.desc = self.desc.lower()
+
         self.solved = False
         self.password = ""
+
+        self.feedback = "No Response Yet"
+        #Password problems are automated
         if ((self.desc.find("password change") != -1) or (self.desc.find("passwordchange") != -1)):
             Ticket.open_tickets -= 1
             Ticket.closed_tickets += 1
@@ -32,13 +47,37 @@ class Ticket():
             self.password = self.staff_id[:3] + self.ticket_creator_name[:3]
 
     def getStatus(self):
-        return self.solved
+        print("-----Ticket Status-----")
+        print("Ticket Number:   ",self.ticket_number)
+        print("Ticket Creator:  ",self.ticket_creator_name)
+        print("Staff Id:        ", self.staff_id)
+        print("Email Address:   ", self.contact_email)
+        print("Description:     ", self.desc)
+        print("Response:        ", self.feedback)
+        print("Ticket Status:   ", self.solved)
 
-test = Ticket("20230721", "Nathaniel", "Email", "password change")
-test1 = Ticket("20230721", "Nathaniel", "Email", "aaaa")
-test2 = Ticket("20230721", "Nathaniel", "Email", "aaaa")
-test3 = Ticket("20230721", "Nathaniel", "Email", "aaaa")
-test4 = Ticket("20230721", "Nathaniel", "Email", "aaaa")
 
-print(test.password)
-print(Ticket.getOpenTickets())
+
+
+
+
+
+ticketList = []
+
+while 1 == 1:
+    temp_object = Ticket(input("Input the Staff Id: "),
+                         input("Input the Ticket Creator Name: "),
+                         input("Input the Contact email: "),
+                         input("Input the Description of the Event: "))
+    ticketList.append(temp_object)
+    print("---------------")
+    check = input("Another Ticket? n for no, any for yes:  ")
+    if check == "n":
+        break
+
+
+for i in range(len(ticketList)):
+    print(ticketList[i].getStatus())
+
+
+
